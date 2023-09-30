51b6fee3eb43690cf940958e2ffbbc49635d837a
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2023-09-30 18:21:39 -0300

    Fetch data com Next (requisição em API) - #12

---
+++ 10_fetch-data/src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='flex flex-col items-center p-4 bottom-0'>
+      Curso de Next.js &copy; 2023
+    </footer>
+  )
+}
