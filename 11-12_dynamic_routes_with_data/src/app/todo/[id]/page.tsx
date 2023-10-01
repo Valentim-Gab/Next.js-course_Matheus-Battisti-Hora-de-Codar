ddf6c094f88fd4813f995c49cd7ed92927da24b3
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2023-09-30 22:02:05 -0300

    Rotas dinâmicas com dados - #14

--- 11-12_dynamic_routes_with_data/src/app/todo/[id]/page.tsx
+++ 11-12_dynamic_routes_with_data/src/app/todo/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Todo } from '@/interfaces/Todo'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,29 +8,32 @@ interface TodoProps {
   }
 }
 
-export default function Todo({ params }: TodoProps) {
+export default async function Todo({ params }: TodoProps) {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.id}`)
+  const todo: Todo = await data.json()
+
   return (
     <div className='flex flex-col gap-5 items-center p-5 min-h-screen'>
       <Link href='/todo'>Voltar</Link>
-      <h1>Exibindo o todo: { params.id }</h1>
+      <h1>Exibindo o todo: { todo.title }</h1>
 
       <div className='flex flex-col gap-5 items-center p-5'>
         <p>Comentários: la la la...</p>
-        <Link href={`/todo/${params.id}/comments/1`}>
+        <Link href={`/todo/${todo.id}/comments/1`}>
           Detalhes
         </Link>
         <hr />
       </div>
       <div className='flex flex-col gap-5 items-center p-5'>
         <p>Comentários: la la la...</p>
-        <Link href={`/todo/${params.id}/comments/2`}>
+        <Link href={`/todo/${todo.id}/comments/2`}>
           Detalhes
         </Link>
         <hr />
       </div>
       <div className='flex flex-col gap-5 items-center p-5'>
         <p>Comentários: la la la...</p>
-        <Link href={`/todo/${params.id}/comments/3`}>
+        <Link href={`/todo/${todo.id}/comments/3`}>
           Detalhes
         </Link>
         <hr />
